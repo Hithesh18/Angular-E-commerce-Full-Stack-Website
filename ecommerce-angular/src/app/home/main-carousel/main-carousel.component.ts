4a281a77d9765fd1dd6f68a737627ed8cbf1aa4e
Hithesh C H <hitheshch2000@gmail.com>
2024-10-03 20:15:01 +0200

    Timeout

--- ecommerce-angular/src/app/home/main-carousel/main-carousel.component.ts
+++ ecommerce-angular/src/app/home/main-carousel/main-carousel.component.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; // Import RouterModule
+import { homeCarouselData } from '../../../Data/mainCarousel';
 
 @Component({
   selector: 'app-main-carousel',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule], // Add RouterModule to the imports array
   templateUrl: './main-carousel.component.html',
   styleUrl: './main-carousel.component.css'
 })
 export class MainCarouselComponent {
-  carouselData:any
+  carouselData: any;
+  currentSlide = 0;
+  interval: any;
 
-}
+  ngOnInit() {
+    console.log(homeCarouselData); // Log the imported data
+    this.carouselData = homeCarouselData;
+    console.log(this.carouselData); // Log the populated data
+    //this.autoPlay()
+    //this.autoPlay()
+   
+  }
+
+  autoPlay() {
+    setInterval(() => {
+      this.nextSlide();
+    }, 2000);
+  }
+
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.carouselData.length;
+    //console.log('Current Slide:', this.currentSlide); // Check the current slide number
+
+  }
+ 
+  
+} 
